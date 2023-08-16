c9d88c0162618ac087105510e401d64c194a0c6f
sagerock <sage@sagerock.com>
2023-08-16 12:05:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,23 +10,6 @@ import random
 
 from PIL import Image
 
-def _max_width_():
-    max_width_str = f"max-width: 600px;"
-    st.markdown(
-        f"""
-<style>
-    .reportview-container .main .block-container{{
-        {max_width_str}
-    }}
-</style>
-""",
-        unsafe_allow_html=True,
-    )
-
-
-_max_width_()
-
-st.set_page_config()
 
 # When you are working locally set your api keys with this:
 # openai.api_key = os.getenv('OPENAI_API_KEY')
